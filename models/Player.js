5ddbdd335550115bb46403f2ed6ad49e11a30211
Shibin Hussain MK <160138800+SHIBINSOJU@users.noreply.github.com>
2026-01-03 11:21:48 +0530

    Update Player.js

--- models/Player.js
+++ models/Player.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    kills: { type: Number, default: 0 },
-    deaths: { type: Number, default: 0 },
-    hearts: { type: Number, default: 10 }, // Lifesteal default
-    balance: { type: Number, default: 0 }
+    username: String,
+    kills: Number,
+    deaths: Number,
+    hearts: Number,
+    balance: Number,
+    isOnline: { type: Boolean, default: false } // <--- ADD THIS LINE
 });
 
 module.exports = mongoose.model('Player', playerSchema);
